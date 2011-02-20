2778b01f9f46a83ed122f8dc733dd4c4a00870de
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2011-02-20 15:16:39 -0300

    primeiro commit do projeto com bd funfando

---
+++ vendor/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConfirmeFacil::Application.config.secret_token = '4fc49e219f0bdb7519ebb657c2bc96c3a60095587664f2a22b057f10b892dd6c74ab1f6fe2e9ba86b03ab3fe2009136c1e2b74e7fb05e114d0c2015feb3684c2'
